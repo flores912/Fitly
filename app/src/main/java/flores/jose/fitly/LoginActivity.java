a3ea5a12869353a4428c5909c49bc2b1e77df776
flores912 <47996013+flores912@users.noreply.github.com>
2020-09-04 22:28:16 -0600

    sign up step 1 layout added added.

--- app/src/main/java/flores/jose/fitly/LoginActivity.java
+++ app/src/main/java/flores/jose/fitly/LoginActivity.java
@@ -1,35 +1,29 @@
 package flores.jose.fitly;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
@@ -41,6 +35,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+    private void initializeLoginButton(){
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logInWithEmail();
+            }
+        });
     }
     private void initializeFbLoginButton(){
         // Initialize Facebook Login button
@@ -94,7 +97,33 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
+    private void logInWithEmail(){
+        TextInputEditText emailTextInput = findViewById(R.id.emailTextInputLogin);
+        TextInputEditText passwordTextInput = findViewById(R.id.passwordTextInputLogin);
 
+        String email =  emailTextInput.getText().toString();
+        String password = passwordTextInput.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
+}
 
 
     @Override
---
+++ app/src/main/java/flores/jose/fitly/SignUpStep1Fragment.java
@@ -0,0 +1,28 @@
+package flores.jose.fitly;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignUpStep1Fragment extends Fragment {
+
+    public SignUpStep1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up_step1, container, false);
+    }
+}
